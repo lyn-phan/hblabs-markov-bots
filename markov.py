42c7796fbf94ba3c2f56a6b51561838ae123db02
LYNDA PHAN <lynda@lyndas-mbp.lan>
2020-10-07 16:49:20 -0700

    override bot success

--- markov.py
+++ markov.py
@@ -4,7 +4,6 @@ import discord
 import sys
 from random import choice
 
-
 def open_and_read_file(filenames):
     """Take list of files. Open them, read them, and return one long string."""
 
@@ -79,5 +78,5 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('O hai.')
 
-os.environ.get('DISCORD_TOKEN')
+os.environ.get('DISCORD TOKEN')
 client.run(os.environ['DISCORD_TOKEN'])
